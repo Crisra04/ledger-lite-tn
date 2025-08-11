c163942d5fb1f2bbb894233818ab170224fbf0aa
Crisra04 <ccuellar0409@gmail.com>
2025-08-11 07:03:17 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('ll-tn-dyn-v1').then(c=>c.addAll([
-    './','./index.html','./styles.css','./app.js','./manifest.json'
-  ])));
-});
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(caches.match(e.request).then(resp=> resp || fetch(e.request)));
-});
